d277196b523fb657942eb95d9858747d15c8a80b
Oleksandr_Husiev <ohusiev@icloud.com>
2025-05-01 07:15:43 +0200

    fixed backend controller tests to rollback test data from DB

--- backend/src/test/java/com/psyntify/backend/controller/MyPlantControllerTest.java
+++ backend/src/test/java/com/psyntify/backend/controller/MyPlantControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class MyPlantControllerTest {
 
     @Autowired
--- backend/src/test/java/com/psyntify/backend/controller/PublicPlantControllerTest.java
+++ backend/src/test/java/com/psyntify/backend/controller/PublicPlantControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class PublicPlantControllerTest {
 
     @Autowired
